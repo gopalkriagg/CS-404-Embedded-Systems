7f7f158e9b4ce9d031f1b7b69febe338f27658b2
Sanjeev Khare <sanjekhare@gmail.com>
2015-10-17 21:05:58 +0530

    Solved (or Modified solutions of) first three problems

--- labs/lab03/analog.js
+++ labs/lab03/analog.js
@@ -1,15 +1,17 @@
+/****************************************************
+ * Objective:   Read analog values from a potentiometer
+ * Authors:     Gopal Krishan Aggarwal and Sanjeev Khare
+ ****************************************************/
+
 var b = require('bonescript');
 
 var inputPin = "P9_33";
-//var outputPin = "P9_14";
+var time = 100; //in milliseconds
 
-//b.pinMode(outputPin, b.ANALOG_OUTPUT);
-loop();
+setInterval(loop, time);
 
 function loop() {
     var value = b.analogRead(inputPin);
-    console.log(value.toFixed(3));
-    //b.analogWrite(outputPin, value);
-    setTimeout(loop, 100);
+    console.log("Value from potentiometer is %d", value.toFixed(3));
 }
 
--- labs/lab03/potWithFade.js
+++ labs/lab03/potWithFade.js
@@ -1,11 +1,13 @@
 /****************************************************
- * Objective:   Fade 2 Leds in reverse. 
- *              When first brightens up the other dims and vice versa
+ * Objective:   Fade 2 Leds in reverse.
+ *              When first brightens up the other dims and vice versa.
+ *              The max brightness is scaled by value from potentiometer
  * Authors:     Gopal Krishan Aggarwal and Sanjeev Khare
  ****************************************************/
 
-var b = require('bonescript');
 
+var b = require('bonescript');
+var inputPin = "P9_33";
 var led1 = "P9_14";
 var led2 = "P9_16";
 b.pinMode(led1, b.ANALOG_OUTPUT);
@@ -18,6 +20,7 @@ setInterval(fade, 10);
 var awValue = 0.01;
 var awDirection = 1;
 function fade() {
+     var value = b.analogRead(inputPin);
  awValue = awValue + (awDirection * 0.01);
  if (awValue > 1.0) {
   awValue = 1.0;
@@ -27,6 +30,7 @@ function fade() {
   awValue = 0.01;
   awDirection = 1;
  }
- b.analogWrite(led1, awValue);
- b.analogWrite(led2, Math.abs(1-awValue));
+ b.analogWrite(led1, awValue*value);
+ b.analogWrite(led2, Math.abs(1-awValue)*value);
+  console.log("Value from potentiometer is %d", value.toFixed(3));
 }
